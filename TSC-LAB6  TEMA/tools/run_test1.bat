a44aee72f11c020f11badb9089f516c2a32aca4a
Bogdan-Valentin Nuțică <truebogdan1@gmail.com>
2023-04-24 13:42:58 +0300

    check results added to testing scripts

---
+++ TSC-LAB6  TEMA/tools/run_test1.bat
@@ -0,0 +1,11 @@
+call run_test.bat 5 0 665 c test1
+cd ../tools
+call run_test.bat 10 1 111 c test2
+cd ../tools
+call run_test.bat 12 3 65666 c test3 
+cd ../tools
+call run_test.bat 14 2 141234111 c test4
+cd ../tools
+call run_test.bat 10 0 114123411 c test5 
+cd ../tools
+call run_test.bat 5 0 665 c test6
